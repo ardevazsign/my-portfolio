4771a4c566083fef391803d7572853149449f848
owen saavedra <newojunior@gmail.com>
2024-05-23 17:58:59 +0800

    commit partial functionality

---
+++ js/index.js
@@ -0,0 +1,7 @@
+//  lead to my messenger
+const messageOwnerBtn = document.getElementById('messageOwner');
+
+messageOwnerBtn.addEventListener('click', () => {
+  window.open('https://m.me/owen.saavedra.3', '_blank');
+  // window.location.href = 'https://m.me/owen.saavedra.3';
+});
