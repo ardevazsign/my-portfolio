f400fe32478236e32823f082747d1d7c18484cd9
owen saavedra <newojunior@gmail.com>
2024-05-27 23:00:08 +0800

    additional function btn

--- js/script.js
+++ js/script.js
@@ -1,60 +1,127 @@
-// certificate 
-const certHtml = document.querySelector('.html');
-const certCss = document.querySelector('.css');
-const certJavascript = document.querySelector('.javascript');
-const certReact = document.querySelector('.react');
+// Revised May 27, 2024
 
-let certHolder = document.getElementById('.cert-holder')
+const popup = document.getElementById('popup');
+const popupServices = document.getElementById('popup-services');
+const popupOthers = document.getElementById('popup-others');
 
-let contentForm = document.querySelectorAll('.certificate');
+const certHtml = document.getElementById('html');
+const certCss = document.getElementById('css');
+const certJavascript = document.getElementById('javascript');
+const certReact = document.getElementById('react');
 
-// const scrollToAboutBtn = document.querySelectorAll('.close-cert');
+document.getElementById('trigger1').addEventListener('click', () => {
+       popup.style.display = 'block';
+});
+document.getElementById('trigger2').addEventListener('click', () => {
+       popupServices.style.display = 'block';
+});
+document.getElementById('trigger3').addEventListener('click', () => {
+       popupOthers.style.display = 'block';
+});
+document.getElementById('triggerA').addEventListener('click', () => {
+       certHtml.style.display = 'block';
+});
+document.getElementById('triggerB').addEventListener('click', () => {
+       certCss.style.display = 'block';
+});
+document.getElementById('triggerC').addEventListener('click', () => {
+       certJavascript.style.display = 'block';
+});
+document.getElementById('triggerD').addEventListener('click', () => {
+       certReact.style.display = 'block';
+});
 
-const aboutSection = document.getElementById('about');
+document.addEventListener('click', (event) => {
+  if(event.target !== document.getElementById('trigger1')) {
+    popup.style.display = 'none';
+  }
+  if (event.target !== document.getElementById('trigger2')) {
+     popupServices.style.display = 'none';
+  }
+  if (event.target !== document.getElementById('trigger3')) {
+    popupOthers.style.display = 'none';
+  }
+  if (event.target !== document.getElementById('triggerA')) {
+    certHtml.style.display = 'none';
+  }
+  if (event.target !== document.getElementById('triggerB')) {
+    certCss.style.display = 'none';
+  }
+  if (event.target !== document.getElementById('triggerC')) {
+    certJavascript.style.display = 'none';
+  }
+  if (event.target !== document.getElementById('triggerD')) {
+    certReact.style.display = 'none';
+  }
+});
 
-document.querySelector('.close-cert').onclick = () => {
-    certHtml.classList.remove('active');
-    certCss.classList.remove('active');
-    certJavascript.classList.remove('active');
-  certReact.classList.remove('active');
+
+
+let sections = document.querySelectorAll('.section');
+
+let navbarLink = document.querySelectorAll('.nav_menu_list .nav_list .nav-link');
+
+
+
+
+
+
+// const certHtml = document.querySelector('.html');
+// const certCss = document.querySelector('.css');
+// const certJavascript = document.querySelector('.javascript');
+// const certReact = document.querySelector('.react');
+
+// let certHolder = document.getElementById('.cert-holder')
+
+// let contentForm = document.querySelectorAll('.certificate');
+
+// // const scrollToAboutBtn = document.querySelectorAll('.close-cert');
+
+// const aboutSection = document.getElementById('about');
+
+// document.querySelector('.close-cert').onclick = () => {
+//     certHtml.classList.remove('active');
+//     certCss.classList.remove('active');
+//     certJavascript.classList.remove('active');
+//   certReact.classList.remove('active');
   
 
-};
+// };
 
 
 
 
-document.querySelector('#html').onclick = () =>{
-  certHtml.classList.toggle('active');
+// document.querySelector('#html').onclick = () =>{
+//   certHtml.classList.toggle('active');
 
-};
-document.querySelector('#css').onclick = () =>{
-     certCss.classList.toggle('active');
-};
-document.querySelector('#javascript').onclick = () =>{
-     certJavascript.classList.toggle('active');
-};
-document.querySelector('#react').onclick = () =>{
-     certReact.classList.toggle('active');
-};
+// };
+// document.querySelector('#css').onclick = () =>{
+//      certCss.classList.toggle('active');
+// };
+// document.querySelector('#javascript').onclick = () =>{
+//      certJavascript.classList.toggle('active');
+// };
+// document.querySelector('#react').onclick = () =>{
+//      certReact.classList.toggle('active');
+// };
 
-window.onscroll = () => {
+// window.onscroll = () => {
   
    
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+//     sections.forEach(sec => {
+//         let top = window.scrollY;
+//         let offset = sec.offsetTop;
+//         let height = sec.offsetHeight;
+//         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height) {
-            navbarLink.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('.nav_menu_list .nav_list .nav-link[href*=' + id + ']').classList.add('active');    
-            });
-        };
-    });
-};
+//         if(top >= offset && top < offset + height) {
+//             navbarLink.forEach(links => {
+//                 links.classList.remove('active');
+//                 document.querySelector('.nav_menu_list .nav_list .nav-link[href*=' + id + ']').classList.add('active');    
+//             });
+//         };
+//     });
+// };
 
 
 // function myMenuFunction() {
